37456a4e0a88ff8698846d9c5651bfc5326effea
p.krysztofiak <p.krysztofiak@pixel.com.pl>
2022-07-13 13:21:58 +0200

    Add POST mapping

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -66,5 +67,13 @@ public class DesignTacoController {
 	public String showDesignForm() {
 		log.info("showDesignForm()");
 		return "design";
+	}
+	
+	@PostMapping
+	public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+		tacoOrder.addTaco(taco);
+		log.info("Processing taco: {}", taco);
+		
+		return "redirect:/orders/current";
 	}
 }
