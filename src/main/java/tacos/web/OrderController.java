05ae8be21e61bf28b1efe21916ac6fb7ab366846
p.krysztofiak <p.krysztofiak@pixel.com.pl>
2022-07-18 11:59:51 +0200

    Changes

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -1,6 +1,9 @@
 package tacos.web;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +18,21 @@ import tacos.TacoOrder;
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
-	
+
 	@GetMapping("/current")
 	public String orderForm() {
 		return "orderForm";
 	}
-	
+
 	@PostMapping
-	public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+	public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+		if (errors.hasErrors()) {
+			return "orderForm";
+		}
+		
 		log.info("Order submitted: {}", order);
 		sessionStatus.setComplete();
-		
+
 		return "redirect:/";
 	}
 }
