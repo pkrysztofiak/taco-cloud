2b06f1cbc8a4e5726861879fe2fde407c289e3ae
p.krysztofiak <p.krysztofiak@pixel.com.pl>
2022-07-19 10:16:28 +0200

    Add repository

--- src/main/java/tacos/web/IngredientByIdConverter.java
+++ src/main/java/tacos/web/IngredientByIdConverter.java
@@ -8,27 +8,19 @@ import org.springframework.stereotype.Component;
 
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.data.IngredientRepository;
 
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient> {
 
-	private Map<String, Ingredient> ingredientMap = new HashMap<>();
+	private final IngredientRepository ingredientRepository;
 	
-	public IngredientByIdConverter() {
-		ingredientMap.put("FLTO", new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
-		ingredientMap.put("COTO", new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
-		ingredientMap.put("GRBF", new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
-		ingredientMap.put("CARN", new Ingredient("CARN", "Carnitas", Type.PROTEIN));
-		ingredientMap.put("TMTO", new Ingredient("TMTO", "Diced tomatoes", Type.VEGGIES));
-		ingredientMap.put("LETC", new Ingredient("LETC", "Lettuce", Type.VEGGIES));
-		ingredientMap.put("JACK", new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
-		ingredientMap.put("CHED", new Ingredient("CHED", "Cheddar", Type.CHEESE));
-		ingredientMap.put("SLSA", new Ingredient("SLSA", "Salsa", Type.SAUCE));
-		ingredientMap.put("SRCR", new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+	public IngredientByIdConverter(IngredientRepository ingredientRepository) {
+		this.ingredientRepository = ingredientRepository;
 	}
 	
 	@Override
 	public Ingredient convert(String id) {
-		return ingredientMap.get(id);
+		return ingredientRepository.findById(id).orElse(null);
 	}
 }
