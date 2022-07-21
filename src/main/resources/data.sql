3920d9c8392c3917f2a86b92fee41ca3d141f380
p.krysztofiak <p.krysztofiak@pixel.com.pl>
2022-07-21 06:52:54 +0200

    Save TacoOrder and children

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,6 +10,6 @@ insert into Ingredient (id, name, type) values ('CARN', 'Carnitas', 'PROTEIN');
 insert into Ingredient (id, name, type) values ('TMTO', 'Diced Tomatoes', 'VEGGIES');
 insert into Ingredient (id, name, type) values ('LETC', 'Lettuce', 'VEGGIES');
 insert into Ingredient (id, name, type) values ('CHED', 'Cheddar', 'CHEESE');
-insert into Ingredient (id, name, type) values ('JACK', 'Monterre Jack', 'CHEESE');
+insert into Ingredient (id, name, type) values ('JACK', 'Monterrey Jack', 'CHEESE');
 insert into Ingredient (id, name, type) values ('SLSA', 'Salsa', 'SAUCE');
 insert into Ingredient (id, name, type) values ('SRCR', 'Sour Cream', 'SAUCE');
